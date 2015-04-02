3f504a86b3f353795345a380e4d37160b0faafe5
kevinzhow <kevinchou.c@gmail.com>
2015-04-03 02:57:36 +0800

    Fix

---
+++ Interview/Source/InImageView.swift
@@ -0,0 +1,21 @@
+//
+//  InImageView.swift
+//  Interview
+//
+//  Created by kevinzhow on 15/4/3.
+//  Copyright (c) 2015年 kevinzhow. All rights reserved.
+//
+
+import UIKit
+
+class InImageView: UIImageView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Interview/ViewController.swift
+++ Interview/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var inImageView: InImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inImageView = InImageView(frame: CGRectMake(0, 0, self.view.bounds.width, (self.view.bounds.width/16)*9))
+        inImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        self.view.addSubview(inImageView)
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
